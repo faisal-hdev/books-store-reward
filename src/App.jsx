d401d4ba3fa28f818e4e55517cfde9e76c5031b5
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-03-12 22:52:42 +0600

    Dynamically data show to ui and event handle click the button and cart section data appended show the ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Title from "./Title";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [cartItem, setCartItem] = useState([]);
+
+  // load from json-data
+  useEffect(() => {
+    fetch("product.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  const handleAddToCart = (product) => {
+    setCartItem([...cartItem, product]);
+  };
+
   return (
     <>
       <Title />
@@ -10,36 +25,50 @@ function App() {
           id="productContainer"
           className="pt-7 w-full lg:w-[80%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 lg:gap-12"
         >
-          <div className="relative flex w-full h-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-            <div className="relative mx-4 -mt-6 h-52 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40">
-              <img src="" alt="img-blur-shadow" className="h-full w-full" />
-            </div>
-            <div className="p-6">
-              <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased"></h5>
-
-              <div>
-                <span>
-                  <del></del>TK.
-                </span>
-                <span className="ml-3"> TK.</span>
-              </div>
-
-              <div className="rating"></div>
-            </div>
-            <div className="p-6 pt-0">
-              <button
-                className="select-none rounded-lg bg-gradient-to-r to-emerald-600 from-sky-400 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                type="button"
-                data-ripple-light="true"
+          {products?.map((product) => {
+            const { name, image, originalPrice, discountPrice, rating } =
+              product;
+            return (
+              <div
+                key={product?.name}
+                className="relative flex w-full h-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md"
               >
-                Add to cart
-              </button>
-            </div>
-          </div>
+                <div className="relative mx-4 -mt-6 h-52 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40">
+                  <img
+                    src={image}
+                    alt="img-blur-shadow"
+                    className="h-full w-full"
+                  />
+                </div>
+                <div className="p-6">
+                  <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                    {name}
+                  </h5>
+                  <div>
+                    <span>
+                      <del>{originalPrice}</del> TK.
+                    </span>
+                    <span className="ml-3">{discountPrice} TK.</span>
+                  </div>
+                  <div className="rating">Rating : {rating}</div>
+                </div>
+                <div className="p-6 pt-0">
+                  <button
+                    onClick={() => handleAddToCart(product)}
+                    className="select-none rounded-lg bg-gradient-to-r to-emerald-600 from-sky-400 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                    type="button"
+                    data-ripple-light="true"
+                  >
+                    Add to cart
+                  </button>
+                </div>
+              </div>
+            );
+          })}
         </div>
 
         <div className="lg:w-[20%] w-full h-[100vh] overflow-y-auto sticky top-3">
-          <p className="text-center">Total Cart Item</p>
+          <p className="text-center">Total Cart Item : {cartItem.length}</p>
           <p className="text-center p-3" id="totalPriceDisplay"></p>
           <div id="cartContainer" className="cartContainer ">
             <table className="table-auto border w-full text-center">
@@ -50,10 +79,14 @@ function App() {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>name</td>
-                  <td>550</td>
-                </tr>
+                {cartItem.map((singleItem) => {
+                  return (
+                    <tr key={singleItem.id}>
+                      <td>{singleItem.name}</td>
+                      <td>{singleItem.originalPrice}</td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           </div>
